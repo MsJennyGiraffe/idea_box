c9bd02fe2a2a16760ab103f320e556d5cedce6b7
Jennifer <jennifersoden@hotmail.com>
2016-10-03 17:03:04 -0600

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+  def initialize
+    create_ideas
+  end
+
+  def create_ideas
+    puts "Creating Ideas"
+    15.times do
+      Idea.create!(
+        title:   Faker::Company.buzzword,
+        body:    Faker::Hipster.paragraph(4),
+        quality: (rand(0..2))
+      )
+    end
+    puts "Created Ideas"
+  end
+end
+
+Seed.new
