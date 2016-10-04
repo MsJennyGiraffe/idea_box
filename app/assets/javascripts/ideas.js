bc28c6410a5957c19b709553cd2161754c7e6696
Jennifer <jennifersoden@hotmail.com>
2016-10-03 20:11:32 -0600

    Add home controller
    Add AJAX for index
    Can list all ideas
    Add stylesheet for background image
    Add bootstrap

---
+++ app/assets/javascripts/ideas.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+  $.ajax({
+    url: "/api/v1/ideas",
+    method: "GET",
+    dataType: "JSON",
+    success: function(response) {
+      var ideas = response.ideas
+      for (var i=0; i < ideas.length; i++) {
+        $(".ideas").append(
+          "<div class='col-md-6'>" +
+          "<h2 class='text-center'>" +
+          ideas[i].title +
+          "</h2>" +
+          "<h4>" +
+          ideas[i].body +
+          "</h4>" +
+          "</div>"
+        );
+      }
+    }
+
+  })
+});
